9b9e5cf5e0d48343fc6b6efde504b6c19956c540
batmandal <145634865+batmandal@users.noreply.github.com>
2023-11-10 13:25:20 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -6,11 +6,21 @@ const card = document.querySelectorAll(".card");
 const container = document.querySelector(".container");
 const counter = document.querySelectorAll(".counter");
 const search = document.querySelector("#search");
+const clicked = document.querySelector(".clicked");
+const submit = document.querySelectorAll("button");
 let editid = 0;
 let editTask = 0;
 
+const elements = form.elements;
+
 let data = JSON.parse(localStorage.getItem("data")) ?? [];
 let id = localStorage.getItem("id") ?? 0;
+
+let searchValue = "";
+let setSearchValue = (newSearchValue) => {
+  searchValue = newSearchValue;
+  render();
+};
 
 const setData = (arr) => {
   data = arr;
@@ -22,24 +32,29 @@ const render = () => {
     card.innerHTML = "";
   });
 
-  const sorted = data.sort((a, b) => {
-    const sa = a.priority === "high" ? 0 : a.priority === "medium" ? 1 : 2;
-    const sb = b.priority === "high" ? 0 : b.priority === "medium" ? 1 : 2;
+  // const sorted = data.sort((a, b) => {
+  //   const sa = a.priority === "high" ? 0 : a.priority === "medium" ? 1 : 2;
+  //   const sb = b.priority === "high" ? 0 : b.priority === "medium" ? 1 : 2;
 
-    return sa - sb;
-  });
+  //   return sa - sb;
+  // });
 
-  sorted.forEach((item) => {
-    if (item.status === "To do") {
-      cards[0].innerHTML += Card(item);
-    } else if (item.status === "In progress") {
-      cards[1].innerHTML += Card(item);
-    } else if (item.status === "Stuck") {
-      cards[2].innerHTML += Card(item);
-    } else if (item.status === "Done") {
-      cards[3].innerHTML += Card(item);
-    }
-  });
+  data
+    // .filter((item) => {
+    //   // return item.title.includes(searchValue);
+    // })
+    .forEach((item) => {
+      if (item.status === "To do") {
+        cards[0].innerHTML += Card(item);
+      } else if (item.status === "In progress") {
+        cards[1].innerHTML += Card(item);
+      } else if (item.status === "Stuck") {
+        cards[2].innerHTML += Card(item);
+      } else if (item.status === "Done") {
+        cards[3].innerHTML += Card(item);
+      }
+      console.log(item.title);
+    });
 
   const deletetask = document.querySelectorAll(".deletetask");
   deletetask.forEach((item) => {
@@ -97,6 +112,9 @@ const render = () => {
       modal.querySelector("#browsers1").value = editPrio;
 
       editTask = 1;
+      clicked.style.display = "flex";
+      container.style.opacity = "0.7";
+      console.log(editTask);
     });
   });
 
@@ -105,6 +123,21 @@ const render = () => {
     dragable.addEventListener("dragstart", (event) => {
       event.dataTransfer.setData("text", event.target.id);
     });
+  });
+  submit.forEach((a) => {
+    a.addEventListener("click", () => {
+      clicked.style.display = "none";
+      container.style.opacity = "1";
+    });
+  });
+  clicked.addEventListener("click", () => {
+    clicked.style.display = "none";
+    modal.style.display = "none";
+    container.style.opacity = "1";
+  });
+
+  search.addEventListener("input", (event) => {
+    setSearchValue(event.target.value);
   });
 };
 
@@ -138,19 +171,21 @@ const Card = (props) => {
 
 render();
 
-addBtn.forEach((a) => {
-  a.addEventListener("click", () => {
-    modal.style.display = "flex";
-    container.style.opacity = "0.7";
-  });
-});
+// addBtn.forEach((a) => {
+//   a.addEventListener("click", () => {
+//     modal.style.display = "flex";
+//     container.style.opacity = "0.7";
+//     clicked.style.display = "flex";
+//   });
+// });
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   if (editTask == 0) {
     const { elements } = event.target;
-    console.log(elements);
+    // console.log(elements);
+
     const desc = elements["desc"].value;
     const title = elements["title"].value;
     const status = elements.status.value;
@@ -158,7 +193,7 @@ form.addEventListener("submit", (event) => {
     const newData = [...data, { title, desc, status, id: "id" + id, priority }];
     id++;
     setData(newData);
-    inputclear(elements);
+
     savedata();
     container.style.opacity = "1";
     counters();
@@ -167,26 +202,25 @@ form.addEventListener("submit", (event) => {
     card.forEach((item) => {
       console.log(item.id);
       const newData = data.map((item) => {
-        // if (item.id === id) {
-        //   item.status = "Done";
-        // }
         if (item.id == editid) {
-          // console.log(item.querySelector("h3").textContent);
-          // item.title.textContent = form.elements.title.value;
           item.title = form.elements.title.value;
           item.desc = form.elements.desc.value;
           item.status = form.elements.status.value;
           item.priority = form.elements.priority.value;
+          // inputclear(elements);
+          editTask = 0;
         }
         return item;
       });
       counters();
+      // inputclear(elements);
       setData(newData);
       savedata();
+      console.log(editTask);
     });
   }
   id++;
-
+  inputclear(elements);
   modal.style.display = "none";
 });
 
@@ -223,7 +257,7 @@ const counters = () => {
 counters();
 
 function inputclear(elements) {
-  for (let i = 0; i < 4; i++) {
+  for (let i = 0; i < 2; i++) {
     elements[i].value = "";
   }
 }
@@ -237,13 +271,23 @@ const savedata = () => {
 const exittask = document.querySelector(".exitaddtask");
 exittask.addEventListener("click", () => {
   modal.style.display = "none";
+  clicked.style.display = "none";
   container.style.opacity = "1";
 });
 
-search.addEventListener("input", (event) => {
-  const value = event.target.value;
-  const newData = data.filter((item) => {
-    return item.title.includes(value);
+addBtn.forEach((a) => {
+  a.addEventListener("click", () => {
+    modal.style.display = "flex";
+    container.style.opacity = "0.7";
+    clicked.style.display = "flex";
   });
-  setData(newData);
 });
+// submit.forEach((a) => {
+//   a.addEventListener("click", () => {
+//     clicked.style.display = "none";
+//     container.style.opacity = "1";
+//   });
+// });
+// search.addEventListener("input", (event) => {
+//   setSearchValue(event.target.value);
+// });
